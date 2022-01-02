b7b5a442928c2c004ad4306fb4281ef6e8a95f45
tlisowicz <tomek2000.2000@tlen.pl>
2022-01-02 14:50:19 +0100

    adding multithreading problem

--- multithreading problem/library/src/main/java/pl/edu/agh/kis/pz1/Library.java
+++ multithreading problem/library/src/main/java/pl/edu/agh/kis/pz1/Library.java
@@ -1,8 +1,46 @@
 package pl.edu.agh.kis.pz1;
 
+/**
+ * This class shares the resource, that can be red by reader or can be written to by a writer
+ */
 public class Library {
 
+    private int resource;
+    /**
+     * main method, showing working algorithm
+     */
     public static void main(String [] args) {
-        Writer writer = new Writer();
+
+        Library library = new Library();
+
+        Writer writer0 = new Writer("Tomasz");
+        Writer writer1 = new Writer("Jakub");
+        Writer writer2 = new Writer("Piotr");
+
+        Reader reader0 = new Reader("Paulina");
+        Reader reader1 = new Reader("Julia");
+        Reader reader2 = new Reader("Wojciech");
+        Reader reader3 = new Reader("Jan");
+        Reader reader4 = new Reader("Karolina");
+        Reader reader5 = new Reader("Karol");
+        Reader reader6 = new Reader("Krzysztof");
+        Reader reader7 = new Reader("Paweł");
+        Reader reader8 = new Reader("Bartosz");
+        Reader reader9 = new Reader("Dawid");
+
+        writer0.start();
+        writer1.start();
+        writer2.start();
+
+        reader0.start();
+        reader1.start();
+        reader2.start();
+        reader3.start();
+        reader4.start();
+        reader5.start();
+        reader6.start();
+        reader7.start();
+        reader8.start();
+        reader9.start();
     }
 }
--- multithreading problem/writer/src/main/java/pl/edu/agh/kis/pz1/Writer.java
+++ multithreading problem/writer/src/main/java/pl/edu/agh/kis/pz1/Writer.java
@@ -2,4 +2,35 @@ package pl.edu.agh.kis.pz1;
 
 public class Writer extends Thread {
 
+    private String name;
+
+    public Writer(String name) {
+        this.name = name;
+    }
+
+    public synchronized void entersTheLibrary() {
+        System.out.println(name + " enters the library.");
+    }
+
+    public synchronized void write() {
+        System.out.println(name + " is writing...");
+    }
+
+    public synchronized void leave() {
+        System.out.println(name + " is leaving library.");
+    }
+
+
+    @Override
+    public void run() {
+        do {
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            entersTheLibrary();
+            write();
+        } while (true);
+    }
 }
